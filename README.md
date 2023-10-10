434f957c87e6b4ddbad3838b5df77b167d559da6
Andrzej Pietkiewicz <pietan@me.com>
2023-10-10 17:35:42 +0200

    New initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Todo App
+
+You can:
+- add todos
+- remove todos
+- edit todos
+- filter
+- check/uncheck all or one by one
+- todos are saved on the server
+- app has error handling
---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+import { UserWarning } from './UserWarning';
+import { NewTodo } from './components/NewTodo';
+import { TodoList } from './components/TodoList';
+import { Filter } from './components/Filter';
+import { Errors } from './components/Errors';
+import { TodosProvider } from './providers/TodosProvider/TodosProvider';
+import { ErrorsProvider } from './providers/ErrorsProvider/ErrorsProvider';
+import { NewTodoProvider } from './providers/NewTodoProvider';
+
+export const USER_ID = 11524;
+
+export const App: React.FC = () => {
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
+  return (
+    <ErrorsProvider>
+      <TodosProvider userId={USER_ID}>
+        <NewTodoProvider>
+
+          <div className="todoapp">
+            <h1 className="todoapp__title">todos</h1>
+
+            <div className="todoapp__content">
+
+              <NewTodo />
+
+              <TodoList />
+
+              <Filter />
+
+            </div>
+
+            <Errors />
+
+          </div>
+
+        </NewTodoProvider>
+      </TodosProvider>
+    </ErrorsProvider>
+  );
+};
